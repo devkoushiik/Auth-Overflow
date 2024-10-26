d5c98e35c0b7b98ece6bbe4374d8f4c1a04d4bf6
koushikxahmed <koushikxahmed@gmail.com>
2024-10-26 14:32:01 +0600

    3 router done

--- models/User.js
+++ models/User.js
@@ -5,28 +5,36 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please provide name'],
+    required: [true, "Please provide name"],
     minlength: 3,
     maxlength: 50,
   },
   email: {
     type: String,
     unique: true,
-    required: [true, 'Please provide email'],
+    required: [true, "Please provide email"],
     validate: {
       validator: validator.isEmail,
-      message: 'Please provide valid email',
+      message: "Please provide valid email",
     },
   },
   password: {
     type: String,
-    required: [true, 'Please provide password'],
-    minlength: 6,
+    required: [true, "Please provide password"],
+    minlength: 4,
   },
   role: {
     type: String,
-    enum: ['admin', 'user'],
-    default: 'user',
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  verificationToken: String,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: {
+    type: Date,
   },
 });
 
